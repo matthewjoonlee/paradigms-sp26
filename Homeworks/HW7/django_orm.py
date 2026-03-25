303170e0a40b8f738449184a0f8c001fadfcfe88
Joanna C. S. Santos <jc_joanna@yahoo.com.br>
2026-03-25 13:45:34 -0400

    Added resources for HW7

---
+++ Homeworks/HW7/django_orm.py
@@ -0,0 +1,14 @@
+# Q1.1: Write a query that retrieve all questions: 
+all_questions = # your query here
+
+# Q1.2: Write a query that retrieve all the choices for the question with a primary key equals to 1:
+choices_for_q1 = # your query here
+
+# Q1.3: Write a query that retrieve all the questions published in 2023: 
+questions_in_2023 = # your query here
+
+# Q1.4: Write a query that sum up the total number of votes for all choices for the question with ID = 1: 
+total_votes = # your query here
+
+# Q1.5: Retrieve all questions sorted by publication date in descending order: 
+questions_desc_order = # your query here
