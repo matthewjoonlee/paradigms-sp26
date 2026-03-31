268ab1ab642eab4d662490661d7f340843bd9852
Matthew Lee <mattlee0601@gmail.com>
2026-03-31 10:16:14 -0400

    hw7: django_orm.py

--- Homeworks/HW7/django_orm.py
+++ Homeworks/HW7/django_orm.py
@@ -1,14 +1,18 @@
+from django.db.models import Sum
+from polls.models import Question, Choice
+
 # Q1.1: Write a query that retrieve all questions: 
-all_questions = # your query here
+all_questions = Question.objects.all()
 
 # Q1.2: Write a query that retrieve all the choices for the question with a primary key equals to 1:
-choices_for_q1 = # your query here
+choices_for_q1 = Choice.objects.filter(question_id=1)
 
 # Q1.3: Write a query that retrieve all the questions published in 2023: 
-questions_in_2023 = # your query here
+questions_in_2023 = Question.objects.filter(pub_date__year=2023)
 
 # Q1.4: Write a query that sum up the total number of votes for all choices for the question with ID = 1: 
-total_votes = # your query here
+total_votes = (Choice.objects.filter(question_id=1)
+               .aggregate(total=Sum('votes'))['total'] or 0)
 
 # Q1.5: Retrieve all questions sorted by publication date in descending order: 
-questions_desc_order = # your query here
+questions_desc_order = Question.objects.order_by('-pub_date')
